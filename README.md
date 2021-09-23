b9d5e3be7d92f665016d790e7f6018bd7a6418f7
Sinadin Shan <sinadinshan66@gmail.com>
2021-09-23 18:20:14 +0530

    Not too late for a repo ig

---
+++ README.md
@@ -0,0 +1,3 @@
+# myExpOS
+
+Is this Windows source code or something? I dont know, I don't use Windows.
---
+++ xfs-interface/batch.bat
@@ -0,0 +1,18 @@
+load --os ../spl/spl_programs/os_startup.xsm
+load --init ../expl/user_progs/user_exec.xsm
+load --idle ../expl/user_progs/idle.xsm
+load --exhandler ../spl/spl_programs/exception.xsm
+load --library ../expl/library.lib
+load --int=timer ../spl/spl_programs/timer.xsm
+load --int=disk ../spl/spl_programs/disk_interrupt.xsm
+load --int=console ../spl/spl_programs/console_interrupt.xsm
+load --int=6 ../spl/spl_programs/int6.xsm
+load --int=7 ../spl/spl_programs/sample_int7.xsm
+load --int=9 ../spl/spl_programs/exec.xsm
+load --int=10 ../spl/spl_programs/intTen.xsm
+load --module 0 ../spl/spl_programs/resource_manager.xsm
+load --module 1 ../spl/spl_programs/process_manager.xsm
+load --module 2 ../spl/spl_programs/memory_manager.xsm
+load --module 4 ../spl/spl_programs/device_manager.xsm
+load --module 5 ../spl/spl_programs/scheduler.xsm
+load --module 7 ../spl/spl_programs/boot_module.xsm
