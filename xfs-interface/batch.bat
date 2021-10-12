2f6866ec3db8cf83dae3b233c0e3fb39dcc843a3
Sinadin Shan <sinadinshan66@gmail.com>
2021-10-12 12:42:36 +0530

    stage 20 completed

--- xfs-interface/batch.bat
+++ xfs-interface/batch.bat
@@ -1,5 +1,5 @@
 load --os ../spl/spl_programs/os_startup.xsm
-load --init ../expl/user_progs/user_exec.xsm
+load --init ../expl/user_progs/forkList.xsm
 load --idle ../expl/user_progs/idle.xsm
 load --exhandler ../spl/spl_programs/exception_handler.xsm
 load --library ../expl/library.lib
@@ -8,8 +8,9 @@ load --int=disk ../spl/spl_programs/disk_interrupt.xsm
 load --int=console ../spl/spl_programs/console_interrupt.xsm
 load --int=6 ../spl/spl_programs/int6.xsm
 load --int=7 ../spl/spl_programs/sample_int7.xsm
+load --int=8 ../spl/spl_programs/fork.xsm
 load --int=9 ../spl/spl_programs/exec.xsm
-load --int=10 ../spl/spl_programs/intTen.xsm
+load --int=10 ../spl/spl_programs/exit.xsm
 load --module 0 ../spl/spl_programs/resource_manager.xsm
 load --module 1 ../spl/spl_programs/process_manager.xsm
 load --module 2 ../spl/spl_programs/memory_manager.xsm
